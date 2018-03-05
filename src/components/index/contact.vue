962eb6d3b75eb78b4c9aeda0ffa713fd9346ee12
Hung Nguyen <viethungax@gmail.com>
2018-03-05 12:28:16 +0700

    added contact blocks and email modal

--- src/components/index/contact.vue
+++ src/components/index/contact.vue
@@ -3,11 +3,41 @@
     <div class="contact-title">
       <span>Contact me</span>
     </div>
+    <div class="contacts">
+      <a href="https://fb.com/ZeroXCEH">
+        <div class="contact-block">
+          <i class="fa fa-facebook"></i>
+        </div>
+      </a>
+      <a href="https://twitter.com/ZeroX-DG">
+        <div class="contact-block">
+          <i class="fa fa-twitter"></i>    
+        </div>
+      </a>
+      <a href="https://github.com/ZeroX-DG">
+        <div class="contact-block">
+          <i class="fa fa-github"></i>
+        </div>
+      </a>
+      <div class="contact-block" @click="openEmail">
+        <i class="fa fa-envelope"></i>
+      </div>
+    </div>
+    <modal ref="email_modal" />
   </section>
 </template>
 <script>
+import modal from './modal';
 export default {
-  name: 'contact'
+  name: 'contact',
+  components: {
+    modal
+  },
+  methods: {
+    openEmail() {
+      this.$refs.email_modal.openModal();
+    }
+  }
 }
 </script>
 
@@ -30,6 +60,46 @@ export default {
         background: white;
         left: calc((100% - 50%) / 2);
         top: 120%;
+      }
+    }
+  }
+  .contacts {
+    width: 500px;
+    display: flex;
+    margin-top: 150px;
+    justify-content: space-around;
+    margin-left: calc((100% - 500px) / 2);
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+    .contact-block {
+      width: 100px;
+      height: 100px;
+      display: flex;
+      border: 3px solid white;
+      color: white;
+      align-items: center;
+      justify-content: center;
+      font-size: 30px;
+      cursor: pointer;
+      transition: all 0.4s;
+      &:hover {
+        background: white;
+        border-radius: 50%;
+        font-size: 50px;
+      }
+      &:hover .fa-facebook {
+        color: #4267b2;
+      }
+      &:hover .fa-twitter {
+        color: #1da1f2;
+      }
+      &:hover .fa-github {
+        color: black;
+      }
+      &:hover .fa-envelope {
+        color: #8e44ad;
       }
     }
   }
---
+++ src/components/index/modal.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <transition name="fade">
+      <div v-if="open"
+          class="modal-mask" 
+          @click="closeModal()"></div>
+    </transition>
+    <transition name="fade">
+      <div v-if="open"
+          class="email-modal modal">
+        <h1 class="title">Hi! Here is my email</h1>
+        <p class="content email">viethungax@gmail.com</p>
+        <p class="content">
+          Don't hesitate to send me an email
+        </p>
+        <p class="content">
+          I bite sometimes but not you :)
+        </p>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  name: 'modal',
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    closeModal() {
+      this.open = false;
+    },
+    openModal() {
+      this.open = true;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.modal-mask {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+}
+.modal {
+  width: 60%;
+  background: white;
+  border-radius: 15px;
+  padding: 50px;
+  box-sizing: border-box;
+  color: black;
+  font-family: "Nunito", sans-serif;
+  margin-left: calc((100% - 60%) / 2);
+  position: fixed;
+  top: 20%;
+  .title {
+    font-size: 50px;
+  }
+
+  .content {
+    font-size: 30px;
+    margin-top: 30px;
+  }
+
+  .email {
+    background: #282828;
+    color: white;
+    padding: 20px 50px;
+    margin-left: -50px;
+    margin-right: -50px;
+  }
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
